6c01c91b640a1b6d0897dda804e6237c70417697
davidvelascogarcia <davelasc@ing.uc3m.es>
2020-07-25 22:54:13 +0200

    Fix format and prevent error timeout exceded

--- programs/speechRecognition.py
+++ programs/speechRecognition.py
@@ -4,20 +4,19 @@
  *      Type: Python
  *      Author: David Velasco Garcia @davidvelascogarcia
  * ************************************************************
- */
-
-/*
-  *
-  * | INPUT PORT                           | CONTENT                                                 |
-  * |--------------------------------------|---------------------------------------------------------|
-  * | /speechRecognition/data:i            | Input audio to recognize                                |
-  *
-  * | OUTPUT PORT                          | CONTENT                                                 |
-  * |--------------------------------------|---------------------------------------------------------|
-  * | /speechRecognition/data:o            | Recognized output text                                 |
-  *
+ *
+ * | INPUT PORT                           | CONTENT                                                 |
+ * |--------------------------------------|---------------------------------------------------------|
+ * | /speechRecognition/data:i            | Input audio to recognize                                |
+ *
+ * | OUTPUT PORT                          | CONTENT                                                 |
+ * |--------------------------------------|---------------------------------------------------------|
+ * | /speechRecognition/data:o            | Recognized output text                                 |
+ *
 '''
 
+# Libraries
+import datetime
 import os
 import speech_recognition as sr
 import yarp
@@ -32,109 +31,151 @@ print("*************************************************************************
 
 print("")
 print("Starting system ...")
+print("")
 
 print("")
 print("Loading Speech Recognition module ...")
-
+print("")
 
 print("")
+print("Initializing Speech Recognition engine ...")
+print("")
+
+
 print("")
 print("**************************************************************************")
 print("YARP configuration:")
 print("**************************************************************************")
 print("")
 print("Initializing YARP network ...")
+print("")
 
 # Init YARP Network
 yarp.Network.init()
 
-
 print("")
 print("[INFO] Opening data input port with name /speechRecognition/data:i ...")
+print("")
 
-# Open input tts port
+# Open speechRecognition input tts port
 speechRecognition_inputPort = yarp.Port()
 speechRecognition_inputPortName = '/speechRecognition/data:i'
 speechRecognition_inputPort.open(speechRecognition_inputPortName)
 
-# Create input data bottle
-inputBottle=yarp.Bottle()
+# Create speechRecognition input data bottle
+speechRecognitionInputBottle = yarp.Bottle()
 
 print("")
 print("[INFO] Opening data output port with name /speechRecognition/data:o ...")
+print("")
 
-# Open output tts port
+# Open speechRecognition output tts port
 speechRecognition_outputPort = yarp.Port()
 speechRecognition_outputPortName = '/speechRecognition/data:o'
 speechRecognition_outputPort.open(speechRecognition_outputPortName)
 
-# Create output data bottle
-outputBottle=yarp.Bottle()
+# Create speechRecognition output data bottle
+speechRecognitionOutputBottle = yarp.Bottle()
 
 print("")
-print("Initializing Speech Recognition engine ...")
+print("[INFO] YARP network configured correctly.")
+print("")
+
+print("")
+print("[INFO] System configured correctly at " + str(datetime.datetime.now()) + ".")
+print("")
 
 # Init speechRecognition engine
 speechRecognitionEngine = sr.Recognizer()
 
-exit=0
+# Variable to loopControlListen
+loopControlListen = 0
 
-print("")
-print("")
-print("**************************************************************************")
-print("Processing:")
-print("**************************************************************************")
-
-while exit==0:
+while int(loopControlListen) == 0:
 
     try:
+        print("")
+        print("**************************************************************************")
+        print("Waiting for input request:")
+        print("**************************************************************************")
+        print("")
+        print("[INFO] Waiting for input request at " + str(datetime.datetime.now()) + " ...")
+        print("")
+
         # Get microphone as audio source
         print("")
-        print("Getting system microphone ...")
+        print("[INFO] Getting system microphone ...")
         print("")
+
         with sr.Microphone() as microphoneSource:
 
             # Reduce ambiente noise
-            print("Adjust ambient noise ...")
+            print("")
+            print("[INFO] Adjust ambient noise ...")
+            print("")
+
             speechRecognitionEngine.adjust_for_ambient_noise(microphoneSource, duration=0.2)
 
             # Listen audio source
-            print("Listening ...")
-            audio = speechRecognitionEngine.listen(microphoneSource, timeout=4.0)
-
-            # Recognize audio source
-            print("Recognizing ...")
-            recognizedText = speechRecognitionEngine.recognize_google(audio, language='es-ES')
-
             print("")
-            print("")
-            print("**************************************************************************")
-            print("Results:")
-            print("**************************************************************************")
-            print("")
-            print("[RESULTS] You said: " + recognizedText)
+            print("[INFO] Listening ...")
             print("")
 
-            # Publish recognized text
-            outputBottle.clear()
-            outputBottle.addString(recognizedText)
-            speechRecognition_outputPort.write(outputBottle)
+            try:
+
+                audio = speechRecognitionEngine.listen(microphoneSource, timeout=4.0)
+
+                # Recognize audio source
+                print("")
+                print("[INFO] Recognizing ...")
+                print("")
+
+                recognizedText = speechRecognitionEngine.recognize_google(audio, language='es-ES')
+
+                print("")
+                print("**************************************************************************")
+                print("speechRecognition results:")
+                print("**************************************************************************")
+                print("")
+                print("[RESULTS] Listened: " + str(recognizedText) + " at " + str(datetime.datetime.now()) + ".")
+                print("")
+
+                # Publish recognized text
+                speechRecognitionOutputBottle.clear()
+                speechRecognitionOutputBottle.addString(recognizedText)
+                speechRecognition_outputPort.write(speechRecognitionOutputBottle)
+                
+            except:
+                print("")
+                print("**************************************************************************")
+                print("speechRecognition results:")
+                print("**************************************************************************")
+                print("")
+                print("[ERROR] Error void audio, timeout limit.")
+                print("")
 
     except sr.RequestError as e:
+
+        print("")
+        print("**************************************************************************")
+        print("speechRecognition results:")
+        print("**************************************************************************")
         print("")
         print("[ERROR] Error, Request Google Speech API.")
         print("")
-        print("**************************************************************************")
 
     except sr.UnknownValueError:
+
+        print("")
+        print("**************************************************************************")
+        print("speechRecognition results:")
+        print("**************************************************************************")
         print("")
         print("[ERROR] Unknown Error")
         print("")
-        print("**************************************************************************")
-
 
 # Close YARP ports
-print("Closing YARP ports ...")
+print("[INFO] Closing YARP ports ...")
 speechRecognition_inputPort.close()
 speechRecognition_outputPort.close()
 
@@ -143,3 +184,6 @@ print("")
 print("**************************************************************************")
 print("Program finished")
 print("**************************************************************************")
+print("")
+print("speechRecognition program finished correctly.")
+print("")
